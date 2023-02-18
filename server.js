4a2d11d2e00cda9c99782aaf1d206ba40f24243c
Blazkowitcz <bonnet.nicolas66@gmail.com>
2023-02-18 23:22:20 +0100

    Add routes and database init

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const app = express();
 const config = require('./config.json');
 const path = require('path');
 
+require('./app/modules/database.module');
+
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.use(cors());
+
+/**
+ * Routes
+ */
+require('./app/routes/auth.route')(app);
 
 app.listen(config.port || 3000, function() {});
