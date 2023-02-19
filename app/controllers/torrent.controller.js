0c30cc5eca9d734f4142679e0dbd285ec70b0ce0
Blazkowitcz <bonnet.nicolas66@gmail.com>
2023-02-19 23:32:09 +0100

    Improve torrent module

--- app/controllers/torrent.controller.js
+++ app/controllers/torrent.controller.js
@@ -1,4 +1,6 @@
 const transmission = require('../modules/torrent.module');
+const fs = require('fs');
+const config = require('../../config.json');
 
 /**
  * Get all torrents
@@ -6,7 +8,20 @@ const transmission = require('../modules/torrent.module');
  * @param {Response} res 
  */
 exports.getTorrents = async (req, res) => {
-    res.send(await transmission.getTorrents());
+    try{
+        const torrents = [];
+        transmission.get(function(err, arg){
+            arg.torrents.forEach(t => {
+                const torrent = {};
+                torrent.name = t.name;
+                torrent.percentage = t.percentDone * 100;
+                torrents.push(torrent);
+            });
+            res.send(torrents);
+        });
+    } catch (e){
+        res.send(false);
+    }
 };
 
 /**
@@ -15,5 +30,43 @@ exports.getTorrents = async (req, res) => {
  * @param {Response} res 
  */
 exports.getTorrentInfo = async (req, res) => {
-    res.send(await transmission.getTorrentInfo(req.body.torrent_id));
+    try{
+        transmission.get(function(err, arg){
+            let torrent = {};
+            arg.torrents.forEach(t => {
+                if (t.id.toString() === req.body.torrent_id){
+                    t.percentDone = t.percentDone * 100;
+                    torrent = t;
+                }
+            });
+            res.send(torrent);
+        });
+    } catch (e){
+        res.send(false);
+    }
+};
+
+/**
+ * Add a torrent file
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+exports.addTorrent = async (req, res) => {
+    const torrent_file = req.files.torrent;
+    await torrent_file.mv('./public/torrents/tmp.torrent');
+    transmission.addFile('./public/torrents/tmp.torrent', function(err, arg){
+        fs.unlink('./public/torrents/tmp.torrent', (err) => {});
+    });
+    res.send(true);
+};
+
+/**
+ * Remove a torrent
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+exports.removeTorrent = async (req, res) => {
+    const remove_data = req.body.remove_data !== undefined ? req.body.remove_data : false;
+    await transmission.removeTorrent(req.body.torrent_id, remove_data);
+    res.send(true);
 };
